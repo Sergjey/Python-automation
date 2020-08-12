77b95dcafefbb06a680f53e53ad31b04325f4ac1
Sergjey <Strongestiq@gmail.com>
2020-08-12 23:37:36 +0300

    a little update with comments

--- Module_1/get_method.py
+++ Module_1/get_method.py
@@ -13,7 +13,8 @@ time.sleep(5)
 driver.get("https://stepik.org/lesson/25969/step/12")
 time.sleep(5)
 
-# Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
+# Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему.
+# Способы поиска элементов мы обсудим позже
 # Ищем поле для ввода текста
 textarea = driver.find_element_by_css_selector(".textarea")
 
