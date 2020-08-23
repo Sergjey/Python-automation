43073ede17e8553a8e6e36298813dcc73a1eb420
Sergjey <Strongestiq@gmail.com>
2020-08-23 20:28:08 +0300

    comment added

--- Module_3/test_fixture10.py
+++ Module_3/test_fixture10.py
@@ -27,3 +27,4 @@ class TestMainPage1():
     def test_guest_should_see_search_button_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("button.favorite")
+#pytest -rx -v test_fixture10.py
