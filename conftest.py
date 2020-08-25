a5b861ad1a7dde10e3faf495d5c9489dd3eec6eb
Sergjey <Strongestiq@gmail.com>
2020-08-25 12:20:35 +0300

    es and fr added in help

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="None",
-                     help="Choose language: ru, en")
+                     help="Choose language: ru, en, es, fr")
 
 
 @pytest.fixture(scope="function")
