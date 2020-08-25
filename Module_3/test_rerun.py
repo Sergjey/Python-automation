b67531efd5a0d0af706d835b1da7557a9165d2dc
Sergjey <Strongestiq@gmail.com>
2020-08-25 11:51:39 +0300

    comment added

--- Module_3/test_rerun.py
+++ Module_3/test_rerun.py
@@ -7,3 +7,6 @@ def test_guest_should_see_login_link_pass(browser):
 def test_guest_should_see_login_link_fail(browser):
     browser.get(link)
     browser.find_element_by_css_selector("#magic_link")
+
+#pytest -v --tb=line --reruns 1 --browser_name=chrome test_rerun.py
+#1 passed 1 failed 1 rerun
